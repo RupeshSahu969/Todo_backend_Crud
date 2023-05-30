6aabda0d43424e82711c6ca0101863e84099157e
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2023-05-30 13:46:45 +0530

    add

--- database/db.js
+++ database/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGO_URL);
+// const connection = mongoose.connect("mongodb+srv://rupeshsahukumar80:Rupeshsahu%401234@cluster0.i9cbi61.mongodb.net/Todoapp?retryWrites=true&w=majority");
+
+const connection=mongoose.connect("mongodb+srv://rupeshsahukumar80:Rupeshsahu%401234@cluster0.i9cbi61.mongodb.net/Tododapp?retryWrites=true&w=majority")
 
 module.exports = {
   connection,
--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const  Express  = require("express");
-const { connection } = require("./database/db");
-const {todoRoute}=require("./Routes/TotoRoute")
+const Express = require("express");
+const mongoose = require("mongoose");
+
+
+const connection=mongoose.connect("mongodb+srv://rupeshsahu:Rupeshsahu%401234@cluster0.2urlqvt.mongodb.net/Todoapp?retryWrites=true&w=majority")
 const cors = require("cors");
+const { todoRoute } = require("./Routes/TotoRoute");
 const { userRoute } = require("./Routes/UserRoute");
 
 const app = Express();
+app.use(Express.json());
 
-const PORT = 8000
+app.use(cors());
 
-app.use(Express.json())
 
-app.use(cors())
+
+app.get("/", async (req, res) => {
+  
+  res.send("Hello");
+});
 
 app.use("/",todoRoute)
 app.use("/",userRoute)
 
-app.listen(PORT, async() => {
-    try{
-        await connection;
-        console.log("connected To DB successfully")
-        console.log(`Listen Port On ${PORT}`)
-    }
-    catch(err){
-        console.log("Error connect to DB")
-        console.log(err)
 
-    }
-    
-    
-})
+app.listen(8000, async () => {
+  try {
+    await connection;
+    console.log("connected To DB successfully");
+    console.log(`Listen Port On 8080`);
+  } catch (err) {
+    console.log("Error connect to DB");
+    console.log(err);
+  }
+});
